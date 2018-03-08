3d2dc4542e1b0f63100ab1514d7a8bf62fdfab1b
Aaron <diesel1812@users.noreply.github.com>
2018-03-08 16:10:17 +0100

    Add files via upload

---
+++ wordcloud.R
@@ -0,0 +1,24 @@
+library("tm")
+library("wordcloud")
+library("RColorBrewer")
+
+# Write to file dataframe column with text of interest
+# write.table(df$textStr,"wordcloud.txt", sep="\t",row.names=FALSE)
+
+# Or simply read from txt file
+filePath <- 'wordcloud.txt'
+text <- read.delim(filePath, sep = "\t")
+
+docs.corpus <- VCorpus(VectorSource(text))
+docs.corpus <- tm_map(docs.corpus, removePunctuation)
+docs.corpus <- tm_map(docs.corpus, content_transformer(tolower))
+docs.corpus <- tm_map(docs.corpus, function(x) removeWords(x, stopwords("english")))
+tdm <- TermDocumentMatrix(docs.corpus)
+m <- as.matrix(tdm)
+v <- sort(rowSums(m),decreasing = TRUE)
+d <- data.frame(word = names(v),freq=v)
+
+png(".../wordcloud.png", width=1280, height=800)
+wordcloud(words = d$word,freq = d$freq, min.freq=2,max.words=100, random.order=F, rot.per=.15,
+          colors=brewer.pal(8, "Dark2"))
+dev.off()
